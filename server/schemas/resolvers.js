a379a7a5496d04648bedd913b8aba06c84bf3599
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-29 21:35:26 -0700

    added login checks

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,5 @@
-const { attachDirectiveResolvers } = require("apollo-server-express");
-const { listenerCount } = require("../config/connection");
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 
 const resolvers = {
@@ -24,6 +23,23 @@ const resolvers = {
             .populate('friends')
             .populate('thoughts');
         }       
+    },
+    Mutation: {
+        addUser: async (parent, args )=> {
+            const user = await User.create(args);
+            return user;
+        },
+        login: async (parent, { email, password }) =>{
+            const user = await User.findOne({ email });
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }  
+            const correctPw = await user.isCorrectPassword(password); 
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }  
+            return user;      
+        }
     }
 }
 
