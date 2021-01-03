6f9961f874caba2ac0c4d01e20b861ed951cab06
Jani Muhlestein <janimuhlestein@gmail.com>
2021-01-03 02:25:10 -0700

    Added if-env and scripts

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -12,8 +12,7 @@ const ThoughtList = ({ thoughts, title }) => {
                 thoughts.map(thought=> (
                     <div key={thought._id} className = "card mb-3">
                         <p className = "card-header">
-                            {thought.username}
-                            thought on {thought.createdAt}
+                            {thought.username} thought on {thought.createdAt}
                         </p>
                     <div className="card-body">
                         <p>{thought.thoughtText}</p>
